01507e4a31c3fcec272da970ff93f9bbbed964de
Pierre A <pierre@avizou.eu>
2021-05-12 19:24:08 +0200

    Correctly map to page url in GAv4

--- integrations/GA4/PageEventConfig.js
+++ integrations/GA4/PageEventConfig.js
@@ -1,7 +1,8 @@
 const pageEventParametersConfigArray = [
-  { src: "path", dest: "page_location" },
+  { src: "path", dest: "page_path" },
   { src: "referrer", dest: "page_referrer" },
   { src: "title", dest: "page_title" },
+  { src: "url", dest: "page_location" },
 ];
 
 export { pageEventParametersConfigArray };
